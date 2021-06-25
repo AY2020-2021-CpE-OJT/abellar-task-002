c4bed2f8ff29adb12c935e1cf1758fc2876d95f6
Levi Abellar <85912450+leviabellar@users.noreply.github.com>
2021-06-25 13:58:54 +0800

    Connect to MongoDB

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb+srv://user001:user001-mongodb-basics@practice.54zqw.mongodb.net/test?retryWrites=true&w=majority', {useUnifiedTopology: true}, (err, client) => {
+    if (err) throw err;
+    console.log('Database Connected');
+    
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`)); 
